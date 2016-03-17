7b3f8fd3594fc4f8e94300d3f37b49ce67390732
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-03-17 19:01:28 -0400

    3/17/2016

---
+++ assets/javascript/ui.js
@@ -0,0 +1,10 @@
+// alert("ready");
+// GOOGLE MAPS EXAMPLE
+ var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.4862, lng: -74.4518},
+          zoom: 8
+        });
+    };
+    
